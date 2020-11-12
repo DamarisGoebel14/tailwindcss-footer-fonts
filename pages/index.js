2ecf7a6cf6b80dc69b3257db258fb05ea8037500
Damaris <d4maris@devisland.de>
2020-11-12 13:43:56 +0100

    added tailwindcss footer with fonts in config

--- pages/index.js
+++ pages/index.js
@@ -2,49 +2,30 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-      <>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-    <div className="container mx-auto bg-gray-200 my-3">
-
-
-      <div className="max-w-sm rounded overflow-hidden shadow-lg my-10">
-        <img className="w-full" src="https://images.unsplash.com/photo-1605034050580-fc9da340f199?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="Sunset in the mountains"/>
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p className="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-            </p>
-          </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-          </div>
+      <div>
+          <Head>
+            <title>Tailwindcss with Conne</title>
+            <link rel="icon" href="/favicon.ico" />
+              <link rel="stylesheet" href="https://use.typekit.net/zun3eyu.css"/>
+          </Head>
+          <footer className="bg-gray-400 pt-10">
+              <div className="container mx-auto bg-red-400" id="big-container">
+                  <div className="grid grid-cols-4 gap-4">
+                      <div className="flex justify-center">
+                          <div className="font-headline">Hallo ich bin Degular Display</div>
+                      </div>
+                      <div className="flex justify-center">
+                            <div className="font-default">Hallo ich bin Roboto</div>
+                      </div>
+                      <div className="flex justify-center">
+                          <div className="font-button">Hallo ich bin PT Mono</div>
+                      </div>
+                      <div className="flex justify-center">
+                            <div className="font-default font-bold">Hallo ich bin Roboto Bold</div>
+                      </div>
+                  </div>
+              </div>
+          </footer>
       </div>
-
-
-      <div className="bg-red-400 p-4 my-10">
-        <span className="block text-white text-center bg-red-800 px-4 py-2">1</span>
-        <span className="block text-white text-center bg-red-800 px-4 py-2 mt-2">2</span>
-        <span className="block text-white text-center bg-red-800 px-4 py-2 mt-2">3</span>
-      </div>
-
-
-      <div className="flex flex-row bg-gray-200">
-        <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
-        <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
-        <div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
-      </div>
-
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Button
-      </button>
-
-    </div>
-          </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,20 @@
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   purge: {
-    enabled: true,
-    content: ["./pages/**/*.js"]},
+    enabled: false,
+    content: ["./pages/**/*.js"]
+  },
   theme: {
-    container: {
-    center: true,
-      padding: {
-        default: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-      },
-    },
-    extend: {},
+    fontFamily: {
+      'headline': 'degular-display, sans-serif',
+      'default': 'roboto, sans-serif',
+      'button': 'pt-mono, monospace',
+      'subtitle':  'bold roboto, sans-serif',
+
+    }
   },
   variants: {},
   plugins: [],
